c668c85ca02adf699b17994e65e7d4994aebd516
Chunghyun Lim <potterLim0808@gmail.com>
2022-09-18 22:27:01 +0900

    Implement searchword function Modify update function formatting

--- src/WordCRUD.java
+++ src/WordCRUD.java
@@ -93,7 +93,7 @@ public class WordCRUD implements ICRUD {
         String meaning = s.nextLine();
         Word word = list.get(idlist.get(id-1));
         word.setMeaning(meaning);
-        System.out.print("단어가 수정되었습니다. ");
+        System.out.println("단어가 수정되었습니다. ");
     }
 
     public void deleteItem() {
